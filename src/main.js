43b4914cfa01f519cd1cc555d55c688a19f464fe
Paul Woitaschek <woitaschek@gmail.com>
2024-02-20 17:34:04 +0100

    Apply a style to the table

--- src/main.js
+++ src/main.js
@@ -10,3 +10,12 @@ import intro from './intro.md';
 document.body.querySelector(".method").innerHTML = method;
 document.body.querySelector(".mittagsyoga").innerHTML = mittagsyoga;
 document.body.querySelector(".intro").innerHTML = intro;
+
+function applyClassesToTables(containerSelector, ...classes) {
+    const tables = document.querySelectorAll(containerSelector + ' table');
+    tables.forEach(table => {
+        classes.forEach(className => table.classList.add(className));
+    });
+}
+
+applyClassesToTables(".method", "table", "table-borderless");
