83f17574e71af0797c4fccc42ff5bfc9aa466e49
Paul Woitaschek <woitaschek@gmail.com>
2026-05-15 20:49:59 +0200

    Redesign

--- src/main.js
+++ src/main.js
@@ -1,27 +1,14 @@
-import * as mdb from 'mdb-ui-kit';
-
-window.mdb = mdb;
 import './styles.scss';
 
-import { Modal, Ripple, initMDB } from "mdb-ui-kit";
-
-initMDB({ Modal, Ripple });
-
 import method from './content/content.md';
-import mittagsyoga from './content/mittagsyoga.md';
+import kurse from './content/Kurse.md';
 import intro from './content/intro.md';
 import impressum from './content/impressum.md';
 
-document.body.querySelector(".method").innerHTML = method;
-document.body.querySelector(".mittagsyoga").innerHTML = mittagsyoga;
-document.body.querySelector(".intro").innerHTML = intro;
-document.body.querySelector(".impressum-body").innerHTML = impressum;
+document.body.querySelector('.method').innerHTML = method;
+document.body.querySelector('.kurse-content').innerHTML = kurse;
+document.body.querySelector('.intro').innerHTML = intro;
+document.body.querySelector('.impressum-body').innerHTML = impressum;
 
-function applyClassesToTables(containerSelector, ...classes) {
-    const tables = document.querySelectorAll(containerSelector + ' table');
-    tables.forEach(table => {
-        classes.forEach(className => table.classList.add(className));
-    });
-}
-
-applyClassesToTables(".method", "table", "table-borderless");
+const yearEl = document.body.querySelector('.year');
+if (yearEl) yearEl.textContent = new Date().getFullYear();
